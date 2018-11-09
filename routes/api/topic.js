ebea1a99c7bba6078993b8de3a40d96683a06409
Preetham7765 <preethamkowshik@gmail.com>
2018-11-09 12:29:52 -0500

    get buzzes around a user location

--- routes/api/topic.js
+++ routes/api/topic.js
@@ -4,23 +4,45 @@ const cors = require('cors');
 
 // topic model
 const Topic = require('../../model/topic');
+const Author = require('../../model/user');
 
 //  @route GET api/items
 // @desc Get all items
 // @access Public
 router.get('/', cors(),(req,res)=> {
-    Topic.find()
-        .sort({date: -1})
-        .then(users => res.json(users));
+    const latitude = parseFloat(req.query.latitude);
+    const longitude = parseFloat(req.query.longitude);
+    console.log("lat, long ", latitude, longitude);
+    Topic.find({
+            "loc": {
+                $geoWithin : {
+                    $centerSphere: [[longitude, latitude], 3/6378]
+                }
+                
+            
+        }})
+        .then(topics => {
+            console.log("sending topics", topics);
+            res.json(topics);
+        })
+        .catch( error => {console.log("could not find any topics near by")});
 });
 
 //  @route POST api/topics
 // @desc Create a post
 // @access Public
 router.post('/', (req,res)=> {
-    const newTopic = new Topic({title: req.body.title,description: req.body.description,author: req.body.author,
-        location: req.body.location.coordinates, comments:[]});
-    newTopic.save().then(user => res.json(user)).catch(err => console.log(err));
+    Author.findOne({'username': req.body.author})
+    .then(author => {
+        const userLocation = [...req.body.location].map( el => parseFloat(el));
+        console.log("location",userLocation);
+        const newTopic = new Topic({title: req.body.title,description: req.body.description, author: author,
+            loc: { type: 'Point', coordinates: userLocation } , comments:[]});
+        // console.log(newTopic);
+        newTopic.save().then(user => res.json(user)).catch(err => console.log(err));
+    })
+    .catch(err => {console.log("Cannot find author" , req.body.author)});
+    
 });
 
 //  @route Delete api/items/:id
