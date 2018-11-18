4c05105aed9fda4fb10a269cacb51f175868c30a
cpatil <cpatil@iu.edu>
2018-11-18 02:20:39 -0500

    save comments

--- routes/api/comment.js
+++ routes/api/comment.js
@@ -4,29 +4,102 @@ const cors = require('cors');
 
 const Comment = require('../../model/comment');
 const User = require('../../model/user');
+const Author = require('../../model/user');
+const Topic = require('../../model/topic');
 
 router.post('/', cors(), (request, response) => {
-    const newComment =
+
+    Author.findById(request.body.authorId)
+    .then((author) => {
+        if(author == null)
+            throw new Error("Could not find author");
+        
+        return Promise.all([ Topic.findById(request.body.topicId), author]);
+    })
+    .then( ([topic, author]) => {
+        const newComment =
         new Comment(
             {
-                author: request.body.author,
-                description: request.body.description,
+                author: author._id,
+                description: request.body.comment,
             });
 
-    newComment
-        .save()
-        .then(comment => response.json(comment))
-        .catch(err => console.log(err));
+        return Promise.all([newComment.save(), topic])
+        
+    })
+    .then(([comment, topic])=> {
+        console.log("Comment",comment);
+        topic.comments.push(comment._id);
+        console.log("topic",topic);
+        topic.save();
+        response.status(200);
+        response.statusMessage ="OK";
+        response.end()
+    })
+    .catch((error)=> {
+        console.log("error",error);
+        response.status(400);
+        response.statusMessage = "Malformed data";
+        reponse.end()
+
+    });
 });
 
-router.get('/:pageNum', cors(), (request, response) => {
+router.get('/:topicId', cors(), (request, response) => {
     const itemsPerRequest = 2;
+    const topicId = request.params.topicId;
+    console.log(topicId);
+    respMsg = [];
+    Topic.findOne({"_id":topicId})
+    .populate({path:'comments', options: {sort : { 'date': "descending" }}})
+    .exec()
+    .then( topic => {
+
+        const asyncLoop = (itr, callback) => {
+
+            if ( itr < topic.comments.length){
+
+                comment = topic.comments[itr];
+                Author.findById(comment.author)
+                .then( author => {
+                    respMsg.push({                
+                    _id: comment._id,
+                    text: comment.description,
+                    createdAt: comment.date,
+                    user : {
+                        _id: comment.author,
+                        name: author.username,
+                        avtar: '' 
+                        }
+                    });
+                    asyncLoop(itr+1 , callback);
+                });
+            }
+            else {
+                callback();
+            }
+        }
+
+        asyncLoop(0, () => {
+            console.log("Response Message ", respMsg);
+            response.status(200)
+            response.json(respMsg);
+        });
+    })
+    .catch( error => {
+        console.log("Error", error);
+        response.status(404);
+        response.statusMessage = "Topic not found";
+        reponse.end()
+    });
+
+    /*
     Comment.find()
         .sort({ votes : -1 })
         .skip(itemsPerRequest * (request.params.pageNum - 1))
         .limit(itemsPerRequest)
         .then(item => response.json(item))
-        .catch(err => console.log(err));
+        .catch(err => console.log(err));*/
 });
 
 router.put('/upvote/:userid/:commentid/', cors(), (request, response) => {
