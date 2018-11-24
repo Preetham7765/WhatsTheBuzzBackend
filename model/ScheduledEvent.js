37defc07e005fbb824d3e582aaad67e34f9650fb
chetan253 <cpatil0@outlook.com>
2018-11-24 14:34:25 -0500

    Added collection for Scheduled Event

---
+++ model/ScheduledEvent.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ScheduledEvent = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    loc: {
+        type: { type: String, enum: "Point", default: "Point"},
+        coordinates: { type: [ Number ] },
+    },
+    region:{
+        type: String,
+    },
+    startAt:{
+        type: Date,
+    },
+    expireAt :{
+        type: Date,
+    },
+    votes: {
+        type: Number,
+        default: 0
+    },
+    votedby:{
+        type: [mongoose.Schema.Types.ObjectId],
+        default:[]
+    },
+    spamCount: {
+        type: Number,
+        default: 0
+    },
+    duplicateCount:{
+        type: Number,
+        default: 0
+    },
+    comments: [{
+        type: Schema.ObjectId, ref: 'comment'
+    }],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+module.exports = Item = mongoose.model('scheduledEvent', ScheduledEvent);
